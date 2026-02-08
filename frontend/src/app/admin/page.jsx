94496caff70aaca452d1cb12b1b0d1547ed6bf0e
Umesh Kumar <umeshkumarceo@gmail.com>
2026-02-08 10:01:43 +0000

    auto-commit for 143852ec-d588-457f-bcdd-e661534e6640

---
+++ frontend/src/app/admin/page.jsx
@@ -0,0 +1,10 @@
+import AdminPage from './AdminClient';
+
+export const metadata = {
+  title: 'Admin | SiteBoard',
+  robots: 'noindex, nofollow',
+};
+
+export default function AdminRoute() {
+  return <AdminPage />;
+}
